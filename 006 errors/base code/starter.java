3933a4a5cf9691b44ff211f4c24ab072e571312f
JacobPooleCV <jpoole@gusd.net>
2021-08-31 08:26:10 -0700

    006 and 007

---
+++ 006 errors/base code/starter.java
@@ -0,0 +1,21 @@
+class starter {
+	public static void main(String args[]) {
+
+		string food = new string ("burritos");
+		string drink = new string ("soda")
+		System.out.println("I love " + food + " and " + drink + "!");
+
+		int count;
+		count = "589";
+		System.out.println("count");
+
+		int sum = 0;
+		double num1 = 12.2;
+		double num2 = 14.3;
+		sum = num1 + num2;
+		
+		System.out.println(sum);
+
+	}
+		System.out.println("I now have 0 errors!!!");		
+}
