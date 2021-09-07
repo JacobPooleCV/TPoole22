313b7c971e401335c8bd0cee517c6191d813a4b2
JacobPoole <jacob.poole589@gmail.com>
2021-09-06 20:47:16 -0700

    13-15

---
+++ 015_Review/base_code/starter.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+import java.util.Random;
+
+class starter {
+	public static void main(String args[]) {
+		// the string "I love to learn coding remotely." will appear in
+		// the command window when you compile and run this program.
+		System.out.print("I love to learn coding remotely."); 
+	}
+}
