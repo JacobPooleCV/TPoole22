084c37bd33cf234060bc7d438463e2c264825b17
JacobPooleCV <jpoole@gusd.net>
2021-09-07 07:24:12 -0700

    error exercise

---
+++ 006 errors/error_exercise/error.java
@@ -0,0 +1,19 @@
+class error {
+	public static void main(String args[]) {
+        
+		system.out.println"Happy vacation day back!";
+        system.out.println"What'd you do this weekend?
+                           I watched a movie!";
+
+		int new number = 125;
+		System.out.println(count);
+
+		int num1 = 12.2;
+		int num2 = 14.3;
+		answer = num1 * num2 + answer;
+		
+		System.out.println(answer)
+
+		System.out.println("I now have 0 errors!!!");		
+	}
+
