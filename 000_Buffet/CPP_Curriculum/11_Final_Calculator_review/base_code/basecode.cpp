60508588915bd3036e61af072a4263198e4177da
JacobPoole <jacob.poole589@gmail.com>
2021-10-25 00:35:40 -0700

    C++ update

---
+++ 000_Buffet/CPP_Curriculum/11_Final_Calculator_review/base_code/basecode.cpp
@@ -0,0 +1,11 @@
+// base code file
+#include "./hfiles/poole.h"
+
+///////////////////////////////////////////////////////////////////////
+
+main(){
+	srand(time(NULL));
+	// write code here
+	cout<<"yeah we are starting c++"<<endl;
+
+}
