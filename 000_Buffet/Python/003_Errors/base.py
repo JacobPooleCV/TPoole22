75bddee709a640b3b74334b342157ab2c911b44b
JacobPooleCV <jpoole@gusd.net>
2022-01-07 13:53:23 -0800

    Python test labs

---
+++ 000_Buffet/Python/003_Errors/base.py
@@ -0,0 +1,11 @@
+    bob = "potato"
+System.out.println("bob")
+
+int joe = 5
+print("joe")
+print("joe is now 5)
+
+cout << "python is pretty cool";
+      String sue = "python rocks!"
+print (sue + "/n + sue)
+print (sue)
