22e62326445ebe1cd0d8786b0548f955182553f2
JacobPoole <jacob.poole589@gmail.com>
2022-04-07 07:45:43 -0700

    remove dup ex

---
+++ 101_Exercises/ArrayList RemoveDup/universal_base_code/starter.java
@@ -0,0 +1,24 @@
+import pkg.*;
+import java.util.*;
+
+class starter {
+	public static void main(String args[]) {
+		ArrayList<Integer> arr1 = new ArrayList<Integer>();
+		addValuesInt(arr1, 100);
+		printValuesInt(arr1);
+		removeDup(arr1);		//Create this method.
+		printValuesInt(arr1);
+	}
+
+	public static void addValuesInt(ArrayList<Integer> arr, int num){
+		for(int i = 0; i < num; i++){
+			arr.add(new Integer((int)(Math.random()*100)));
+		}
+	}
+
+	public static void printValuesInt(ArrayList<Integer> arr){
+		for(int i = 0; i < arr.size(); i++){
+			System.out.print(arr.get(i) + ", ");
+		}
+	}
+}
