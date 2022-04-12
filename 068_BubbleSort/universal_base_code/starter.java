317283d1cf4e21cd24579ae35d63a8cf38003b9f
JacobPooleCV <jpoole@gusd.net>
2022-04-12 12:21:34 -0700

    upload selection update

--- 068_BubbleSort/universal_base_code/starter.java
+++ 068_BubbleSort/universal_base_code/starter.java
@@ -1,14 +1,24 @@
 import pkg.*;
-import java.util.Scanner;
-import java.util.Random;
-
+import java.util.*;
 
 class starter {
 	public static void main(String args[]) {
-		// Your code goes below here
-		BaseClass test = new BaseClass();
+		ArrayList<Integer> arr1 = new ArrayList<Integer>();
+		addValuesInt(arr1, 100);
+		printValuesInt(arr1);
+		bubbleSort(arr1);			//Create this method
+		System.out.println();
+		printValuesInt(arr1);
+	}
 
-
-		
+	public static void addValuesInt(ArrayList<Integer> arr, int num){
+		for(int i = 0; i < num; i++){
+			arr.add(new Integer((int)(Math.random()*10)));
+		}
+	}
+	public static void printValuesInt(ArrayList<Integer> arr){
+		for(int i = 0; i < arr.size(); i++){
+			System.out.print(arr.get(i) + ", ");
+		}
 	}
 }
